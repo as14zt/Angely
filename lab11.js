a6b78c015e2ba92d37c279bb2f48c785c3a5e6ed
Angely Suthaharan <as14zt@github.com>
2017-11-23 19:03:33 -0500

    lab11

---
+++ lab11.js
@@ -0,0 +1,28 @@
+//make a new array for getColour
+var colourArray = new Array();
+colourArray = ["RED" , "GREEN" , "BLUE" , "YELLOW"];
+
+var colour = "";
+
+function getColour(){
+
+var colourNum = Math.round(Math.random()*3)
+colour=String(colourArray[colourNum]);
+console.log(colourArray[colourNum]);
+
+document.getElementById('output').innerHTML = colourArray[colourNum];
+
+setTimeout(function(){document.getElementById('output').innerHTML="";},1000);
+
+}
+function pushButton(userColour){
+
+if(colour == userColour){
+
+document.getElementById('output').innerHTML="CORRECT!";
+setTimeout(getColour(), 1000);
+
+}
+else
+document.getElementById('output').innerHTML="GAME OVER!";
+}
---
+++ lec11.js
@@ -0,0 +1,26 @@
+
+//get largest number
+var theMax = Math.max(10.2, 4.5, 15.6);
+
+//print to the log
+console.log(theMax);
+
+//round number
+var theRound = Math.round(theMax);
+
+//print
+console.log(theRound);
+
+//square root
+var square = Math.sqrt(theRound);
+
+//print
+console.log(square);
+
+//write on page
+document.write(square);
+
+//random numbers
+
+var randomNum = Math.floor(Math.random()*10)+1;
+console.log(randomNum);
